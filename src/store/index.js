1c81e58763ef643cc28b94448c224889bff1b619
teddy <teddymczieuwa@gmail.com>
2018-11-06 02:24:16 +0100

    setup the store

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vuex from 'vuex'
+
+const createStore = () => {
+    return new Vuex.Store({
+        state: {},
+        getters: {},
+        mutations: {},
+        actions: {}
+    })
+}
+
+export default createStore
